7a877d0f73f5e193df0aefeb0cab3bcfe8d52b9a
Oshank Agrawal <128688216+OshankAgrawal@users.noreply.github.com>
2024-03-03 11:06:13 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class calculator {
     public static void main (String arg []){
         try (Scanner obj = new Scanner(System.in)) {
             System.out.print("Enter a number: ");

1df0cc096f7cda85c01d16fb32e5a08fc76ccbeb
Oshank Agrawal <128688216+OshankAgrawal@users.noreply.github.com>
2024-09-09 22:32:38 +0530

    Delete Print_AtoZ.java

--- Print_AtoZ.java
+++
@@ -1,8 +0,0 @@
-public class Print_AtoZ {
-    public static void main (String args[]){
-            for (char num='A'; num<='Z'; num++){
-                //char ch = (char)num;
-                System.out.print(num+" ");
-        }
-    }
-}

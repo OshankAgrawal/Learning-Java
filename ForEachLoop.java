5a04f590d38aa120b9d12a654d786008ce9d7620
Oshank Agrawal <128688216+OshankAgrawal@users.noreply.github.com>
2024-09-09 22:41:42 +0530

    Delete ForEachLoop.java

--- ForEachLoop.java
+++
@@ -1,8 +0,0 @@
-public class ForEachLoop {
-    public static void main(String args[]){
-        int a[]={10,20,30,40,50,60,70,80,90,100};
-        for(int b:a){
-            System.out.println(b);
-        }
-    }
-}

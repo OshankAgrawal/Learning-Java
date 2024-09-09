5e90f855edccae983405d5335d32ea95619ca931
Oshank Agrawal <128688216+OshankAgrawal@users.noreply.github.com>
2024-09-09 22:28:18 +0530

    Delete Ternary Operator.java

--- Ternary Operator.java
+++
@@ -1,7 +0,0 @@
-class Ternary{
-    public static void main (String args[]){
-        int a=10,b=4,c=3,max;
-        max=(a>b) ? (a>c ? a : c) : (b>c ? b : c);
-        System.out.print(max); 
-    }
-}
